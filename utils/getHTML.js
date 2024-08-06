b18960dfa54bcf4ff8c4e5096b830dd7dfc56259
Sukkoth <suukootj@gmail.com>
2024-08-06 11:54:55 +0300

    added more headers

--- utils/getHTML.js
+++ utils/getHTML.js
@@ -9,7 +9,15 @@ async function getHTML() {
     const headers = {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-      // Add more headers if needed
+      "Accept-Language": "en-US,en;q=0.9",
+      "Accept-Encoding": "gzip, deflate, br",
+      Connection: "keep-alive",
+      "Upgrade-Insecure-Requests": "1",
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "none",
+      "Sec-Fetch-User": "?1",
+      "Cache-Control": "max-age=0",
     };
 
     // Send the GET request to the URL
