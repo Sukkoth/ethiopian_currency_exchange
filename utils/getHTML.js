c1a06f38a5fcda548efd9314f7be321612638ce9
Sukkoth <suukootj@gmail.com>
2024-08-06 12:26:38 +0300

    headers

--- utils/getHTML.js
+++ utils/getHTML.js
@@ -7,17 +7,26 @@ async function getHTML() {
   try {
     // Set up headers and cookies if necessary
     const headers = {
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-      "Accept-Language": "en-US,en;q=0.9",
-      "Accept-Encoding": "gzip, deflate, br",
-      Connection: "keep-alive",
-      "Upgrade-Insecure-Requests": "1",
-      "Sec-Fetch-Dest": "document",
-      "Sec-Fetch-Mode": "navigate",
-      "Sec-Fetch-Site": "none",
-      "Sec-Fetch-User": "?1",
-      "Cache-Control": "max-age=0",
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+      "accept-encoding": "gzip, deflate, br, zstd",
+      "accept-language": "en-US,en;q=0.9",
+      "cache-control": "max-age=0",
+      cookie:
+        "nitroCachedPage=0; cf_clearance=bCUVw4gL2cnMQkVQjvy1M5TB5Yfk_gP6sp5tl_wLIOw-1722934599-1.0.1.1-l3Y92Hu.n23Z.HjRSJvzjo9gY0vmCpnZ7JMEjKDjz7Vq2RJKPCUVJtWeWOC4N1sHWAYLDf6Lx_Hsqvbt2vhtYA",
+      dnt: "1",
+      priority: "u=0, i",
+      "sec-ch-ua":
+        '"Not)A;Brand";v="99", "Microsoft Edge";v="127", "Chromium";v="127"',
+      "sec-ch-ua-mobile": "?1",
+      "sec-ch-ua-platform": '"Android"',
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "none",
+      "sec-fetch-user": "?1",
+      "upgrade-insecure-requests": "1",
+      "user-agent":
+        "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36 Edg/127.0.0.0",
     };
 
     // Send the GET request to the URL
