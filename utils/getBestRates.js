dfa190cb0f551460730f0e333e411d39bb61f553
Sukkoth <suukootj@gmail.com>
2024-08-20 14:28:45 +0300

    convert bestRates to number

--- utils/getBestRates.js
+++ utils/getBestRates.js
@@ -4,15 +4,24 @@ function getBestRates($) {
     .find("tbody.fs-13 > tr")
     .each((_, row) => {
       const tds = $(row).find("td");
+      let buyRate = parseFloat($(tds[1]).text().trim());
+      let sellRate = parseFloat($(tds[2]).text().trim());
+
+      let buySellDifference = null;
+      if (buyRate !== null && sellRate !== null) {
+        buySellDifference = sellRate - buyRate;
+      }
+      if (!isNaN(buySellDifference)) {
+        buySellDifference = parseFloat(buySellDifference.toFixed(2));
+      }
+
       bestRateData.push({
         baseCurrency: "ETB",
         currencyCode: $(tds[0]).find("td .trasn .flag p").eq(1).text().trim(), //$('td .trasn .flag p').eq(1).text();
-        buyRate: $(tds[1]).text().trim(),
-        sellRate: $(tds[2]).text().trim(),
-        bank: $(tds[3]).find("a").text().toUpperCase(),
-        buySellDifference: (
-          ($(tds[2]).text().trim() || 0) - ($(tds[1]).text().trim() || 0)
-        ).toFixed(2),
+        bank: $(tds[3]).find("a").text().toUpperCase().trim(),
+        buyRate,
+        sellRate,
+        buySellDifference,
       });
     });
 
